f384c4f21d9669cbce23561b1b7425ff783d7aba
sophistic4ted <sophistic4ted@gmail.com>
2017-03-07 17:48:59 +0100

    Labki2

--- lab2/array2d/Array2D.cpp
+++ lab2/array2d/Array2D.cpp
@@ -4,11 +4,19 @@
 
 #include "Array2D.h"
 
-int **Array2D(int n_rows, int n_columns)
-{
+int **Array2D(int n_rows, int n_columns) {
 
+    int **tab = new int *[n_rows];
+    for (int i = 0; i < n_rows; i++)
+    {
+        tab[i] = new int[n_columns];
+    }
 }
+
 void DeleteArray2D(int **array, int n_rows, int n_columns)
 {
-
+    for (int i=0;i<n_rows;i++){
+        delete array[i];
+    }
+    delete [] array;
 }
