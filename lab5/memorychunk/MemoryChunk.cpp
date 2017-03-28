138e3763e449b2c38fa67ccf91b37b33781dcca5
sophistic4ted <sophistic4ted@gmail.com>
2017-03-28 18:02:21 +0200

    Lab4

--- lab5/memorychunk/MemoryChunk.cpp
+++ lab5/memorychunk/MemoryChunk.cpp
@@ -3,3 +3,39 @@
 //
 
 #include "MemoryChunk.h"
+
+memorychunk::MemoryChunk::MemoryChunk(size_t sz) {
+
+}
+
+int8_t *memorychunk::MemoryChunk::MemoryAt(size_t offset) const {
+    return nullptr;
+}
+
+size_t memorychunk::MemoryChunk::ChunkSize() const {
+    return 0;
+}
+
+memorychunk::MemoryChunk::MemoryChunk(size_t &&sz) {
+
+}
+
+memorychunk::MemoryChunk::MemoryChunk(const size_t &sz) {
+
+}
+
+memorychunk::MemoryChunk &memorychunk::MemoryChunk::operator=(const size_t &sz) {
+    return <#initializer#>;
+}
+
+memorychunk::MemoryChunk &memorychunk::MemoryChunk::operator=(size_t &&sz) {
+    return <#initializer#>;
+}
+
+memorychunk::MemoryChunk::MemoryChunk() {
+
+}
+
+memorychunk::MemoryChunk::~MemoryChunk() {
+
+}
--- lab5/memorychunk/MemoryChunk.h
+++ lab5/memorychunk/MemoryChunk.h
@@ -5,10 +5,29 @@
 #ifndef JIMP_EXERCISES_MEMORYCHUNK_H
 #define JIMP_EXERCISES_MEMORYCHUNK_H
 
+#include <cstdint>
+#include <algorithm>
 
-class MemoryChunk {
+namespace memorychunk{
+    class MemoryChunk {
+        MemoryChunk(size_t sz);
+        MemoryChunk(size_t &&sz);
+        MemoryChunk(const size_t &sz);
+        MemoryChunk &operator = (const size_t &sz);
+        MemoryChunk &operator = (size_t &&sz);
+        MemoryChunk();
 
-};
+
+        int8_t *MemoryAt(size_t offset) const;
+        size_t ChunkSize() const;
+        ~MemoryChunk();
+    private:
+        size_t size;
+        int8_t *ptr;
+    };
+
+}
+
 
 
 #endif //JIMP_EXERCISES_MEMORYCHUNK_H
