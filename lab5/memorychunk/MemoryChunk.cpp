3779ac4e65024931a89597bf4cb027248fa8b0e5
sophistic4ted <sophistic4ted@gmail.com>
2017-04-04 18:07:35 +0200

    Lab4

--- lab5/memorychunk/MemoryChunk.cpp
+++ lab5/memorychunk/MemoryChunk.cpp
@@ -5,7 +5,8 @@
 #include "MemoryChunk.h"
 
 memorychunk::MemoryChunk::MemoryChunk(size_t sz) {
-
+    int8_t* ptr = new int8_t[sz];
+    size = sz;
 }
 
 int8_t *memorychunk::MemoryChunk::MemoryAt(size_t offset) const {
@@ -16,8 +17,8 @@ size_t memorychunk::MemoryChunk::ChunkSize() const {
     return 0;
 }
 
-memorychunk::MemoryChunk::MemoryChunk(size_t &&sz) {
-
+memorychunk::MemoryChunk(MemoryChunk &&sz) : ptr(nullptr) , {
+    std::swap(ptr,sz.ptr);
 }
 
 memorychunk::MemoryChunk::MemoryChunk(const size_t &sz) {
